4d56278f9348dcf348776de68f5f957fe0678c32
SamadhanTikone <99667605+SamadhanTikone@users.noreply.github.com>
2024-11-23 16:03:48 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,97 +5,141 @@ import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import SectionA from "./SectionA";
 import SectionB from "./SectionB";
 import Divider from '@mui/material/Divider';
-import javaImage from './assets/java-svgrepo-com.svg'; // Imported image
-import servicesIcon from './assets/services-svgrepo-com.svg'; // Imported image
+
 
 function Home() {
     const services = [
-        "Advanced Analytics", "Artificial Intelligence", "Automation", "Cloud", "Services",
-        "Business Process", "Connected Intelligence and AI", "Digital Consulting", "Digital CX",
-        "Digital Engineering Services", "Digital Operations", "Linkedin"
-    ];
+        "Advanced Analytics",
 
+        "Artificial Intelligence",
+        "Automation",
+        "Cloud",
+        "Services",
+        "Business Process",
+        "Connected Intelligence and AI",
+        "Digital Consulting",
+        "Digital CX",
+        "Digital Engineering Services",
+        "Digital Operations",
+        "Linkedin",
+    ];
     return (
         <>
-            <div className="mb-24">
-                <div className={`${styles.main} relative`}>
-                    <div className="absolute inset-0 bg-black opacity-60 rounded-[20px]"></div>
+        <div className=" mb-24">
+            <div className={`${styles.main}  relative`}>
+                <div className="absolute inset-0 bg-black opacity-60 rounded-[20px]"></div>
 
-                    <div className="h-screen mt-12 flex justify-between items-center rounded-[20px]">
-                        <div className="z-10">
-                            <span className="block text-6xl font-bold uppercase leading-normal opacity-100 text-white text-left ml-24">
-                                Innovative Solutions for a <span className="text-orange-600 rounded-xl">Digital World</span>
-                            </span>
+                <div className="h-screen mt-12 flex justify-between items-center   rounded-[20px] ">
+                    <div className="  z-10 ">
+                        <span className=" block text-6xl font-bold uppercase leading-normal opacity-100 text-white text-left ml-24">
+                            Innovative Solutions for a <span className="text-orange-600  rounded-xl ">Digital World</span>
+                        </span>
 
-                            <Link to={"/contact"}>
-                                <Button
-                                    variant="contained"
-                                    sx={{
-                                        float: "left",
-                                        margin: "20px 100px",
-                                        borderRadius: "10px",
-                                        padding: "10px 18px"
-                                    }}
-                                >
-                                    Get Started <DoubleArrowIcon />
-                                </Button>
-                            </Link>
-                        </div>
-
-                        <div className="z-10 w-[1200px]">
-                            {/* Corrected path for the image */}
-                            <img src="/vite.svg" alt="Solution" />
-                        </div>
+                        <Link to={"/contact"}>
+                            {" "}
+                            <Button
+                                variant="contained"
+                                sx={{ float: "left", margin: "20px 100px ", borderRadius:"10px", padding:"10px 18px" }}
+                            >
+                                Get Started <DoubleArrowIcon />
+                            </Button>
+                        </Link>
                     </div>
+
+                    <div className="z-10 w-[1200px]">  <img src="src/assets/Business Plan-amico.svg" alt="react" /></div>
                 </div>
 
-                <div className="mt-28 relative h-[80vh] w-full flex justify-center items-center flex-wrap">
+               
+
+            </div>
+
+            <div className="">
+                <div
+                    className={`flex justify-center items-center flex-wrap mt-28 relative h-[80vh] w-full `}
+                >
                     <div>
-                        <h1 className="text-3xl font-bold uppercase block float-left">
+                        <h1 className="text-3xl font-bold uppercase  block  float-left">
                             WE are in Home <span className="text-orange-600">Component,</span>
                         </h1>
-                        <p className="w-full text-left text-lg mt-12">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+                        <p className="w-full  text-left text-lg mt-12">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Praesentium aut id iure alias doloribus et dolor possimus qui
+                            soluta, quidem fugiat libero quasi ea veritatis quod, eligendi
+                            adipisci facilis harum Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Quaerat molestiae aspernatur, non quas est sit
+                            voluptatibus totam asperiores molestias ad illum debitis sapiente
+                            aliquam, distinctio perferendis placeat recusandae. Fugiat
+                            voluptatum ratione quae laborum perferendis error cumque corrupti
+                            asperiores fuga unde? incidunt deserunt? Ratione dolores
+                            dignissimos ad, odit nemo aperiam provident. Lorem ipsum, dolor
+                            sit amet consectetur adipisicing elit. Maxime possimus autem
+                            nesciunt laboriosam fugiat est at nam optio quisquam. Tempore
+                            soluta sequi dignissimos libero fugiat tenetur id excepturi ut,
+                            rem neque ipsam in illum, placeat beatae doloremque, consequuntur
+                            dolores fuga optio. Dolores suscipit quas nisi nostrum. Omnis
+                            fugit nemo cum.
                         </p>
 
-                        {/* Corrected the image source */}
-                        <img src={javaImage} alt="Java" className="z-10 w-24 float-right right-0" />
+                        <img
+                            src="src/assets/java-svgrepo-com.svg"
+                            alt="nasssssssss"
+                            className="z-10 w-24 float-right right-0 "
+                        />
                     </div>
 
-                    <div>
-                        <h2 className="text-3xl font-bold uppercase float-center text-orange-500">
-                            We Provide {" "}
+                    <div className="">
+                        <h2 className=" text-3xl font-bold uppercase  float-centre text-orange-500 ">
+                            We  Provides {" "}
                         </h2>
 
-                        {/* Corrected image source */}
-                        <img src={servicesIcon} alt="" className="w-24 float-left absolute z-10" />
+                        <img
+                            src="src/assets/services-svgrepo-com.svg"
+                            alt=""
 
-                        <div className="flex flex-wrap justify-center items-center">
+                            className=" w-24 float-left absolute  z-10 "
+                        />
+
+                        
+
+                        <div className="flex flex-wrap float-left justify-center items-center">
                             {services.map((service, index) => {
                                 return (
-                                    <div key={index} className="flex flex-col w-1/2 p-2">
-                                        <Link to={"/"} className="text-xl font-semibold text-left ml-56">
-                                            -> {service}
+                                    <div key={index} className="flex flex-col w-1/2  p-2 ">
+                                        <Link
+                                            to={"/"}
+                                            className="text-xl font-semibold text-left ml-56 "
+                                        >
+                                            -&gt; {service}
                                         </Link>
                                     </div>
                                 );
                             })}
                         </div>
-                        <img src={javaImage} alt="" className="z-10 w-24 left-[50%] bottom-0 absolute" />
+                        <img
+                            src="src/assets/java-svgrepo-com.svg"
+                            alt=""
+                            className="z-10 w-24 left-[50%] bottom-0 absolute "
+                        />
                     </div>
                 </div>
             </div>
-
-            <Divider sx={{ borderBlockColor: "white", marginBottom: "10px" }} />
-
-            <div>
-                <h1 className="text-5xl font-bold uppercase mb-4">Top <span className="text-orange-600">Specific</span></h1>
-                <p><SectionA /></p>
-                <Divider sx={{ borderBlockColor: "white", margin: "20px 0px" }} />
-                <p><SectionB /></p>
-                <Divider sx={{ borderBlockColor: "white", margin: "20px 0px" }} />
-            </div>
+           
         </div>
+        <Divider sx={{borderBlockColor:"white", marginBottom:"10px"}}/>
+        <div>
+            <h1 className="text-5xl font-bold uppercase mb-4">Top <span className="text-orange-600">Specific</span></h1>
+        <p className=""> <SectionA/></p>
+        <Divider sx={{borderBlockColor:"white", margin:"20px 0px"}}/>
+        <p className=""> <SectionB/></p>
+        <Divider sx={{borderBlockColor:"white", margin:"20px 0px"}}/>
+        </div>
+
+        <div>
+
+            
+        </div>
+
+      
         </>
     );
 }
